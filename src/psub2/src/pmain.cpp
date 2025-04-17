dfa79b548fdfd91149d26624daa7b3ae75e90f8d
suho2001228 <103232995+suho2001228@users.noreply.github.com>
2025-04-17 17:37:17 +0900

    Delete src/psub2 directory

--- src/psub2/src/pmain.cpp
+++
@@ -1,9 +0,0 @@
-#include "psub2/pub.hpp"
-int main(int argc, char * argv[])
-{
-rclcpp::init(argc, argv);
-auto node = std::make_shared<Pub>();
-rclcpp::spin(node);
-rclcpp::shutdown();
-return 0;
-}
--- src/psub2/src/smain.cpp
+++
@@ -1,9 +0,0 @@
-#include "psub2/sub.hpp"
-int main(int argc, char * argv[])
-{
-rclcpp::init(argc, argv);
-auto node = std::make_shared<Sub>();
-rclcpp::spin(node);
-rclcpp::shutdown();
-return 0;
-}
