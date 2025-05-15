58a163b728c9c9ffda9f29eddd7d6738dde4d9de
suho2001228 <103232995+suho2001228@users.noreply.github.com>
2025-05-15 15:42:18 +0900

    Add files via upload

---
+++ src/camera1-2/src/sub_wsl.cpp
@@ -0,0 +1,41 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/compressed_image.hpp"
+#include "opencv2/opencv.hpp"
+#include <memory>
+#include <functional>
+#include <iostream>
+using std::placeholders::_1;
+
+cv::VideoWriter writer;
+
+void mysub_callback(rclcpp::Node::SharedPtr node, const sensor_msgs::msg::CompressedImage::SharedPtr msg)
+{
+    cv::Mat frame = cv::imdecode(cv::Mat(msg->data),  cv::IMREAD_GRAYSCALE); // 흑백이라 
+    // 프레임 저장
+    if (writer.isOpened())
+        writer << frame;
+    cv::imshow("wsl",frame);
+    cv::waitKey(1);
+    RCLCPP_INFO(node->get_logger(), "Received Image : %s,%d,%d", msg->format.c_str(),frame.rows,frame.cols);
+}
+int main(int argc, char* argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<rclcpp::Node>("camsub_wsl");
+
+    // VideoWriter 초기화 (흑백, 저장 크기 640x360, fps=30)
+    writer.open("output.avi", cv::VideoWriter::fourcc('M','J','P','G'), 30.0, cv::Size(640, 360), false);
+    if (!writer.isOpened()) {
+        RCLCPP_ERROR(node->get_logger(), "Failed to open video writer!");
+        return -1;
+    }
+
+    auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10)).best_effort();
+    std::function<void(const sensor_msgs::msg::CompressedImage::SharedPtr msg)> fn;
+    fn = std::bind(mysub_callback, node, _1);
+    auto mysub = node->create_subscription<sensor_msgs::msg::CompressedImage>("image/compressed",qos_profile,fn);
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
+
